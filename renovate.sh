2f8d618aeeb7f7d721f43563cff688bd65122558
严浩 <me@yanhao.ren>
2024-03-26 15:41:08 +0800

    Add renovate.sh script for automated dependency updates

---
+++ renovate.sh
@@ -0,0 +1,11 @@
+# https://docs.renovatebot.com/modules/platform/gitea/
+# https://www.cnblogs.com/Gitea/p/renovate.html
+
+docker run --rm \
+    --name renovate \
+    -e RENOVATE_AUTODISCOVER=true \
+    -e RENOVATE_ENDPOINT=https://git.yanhao.ren \
+    -e RENOVATE_GIT_AUTHOR='Renovate Bot <gitea-renovatebot@oo1.dev>' \
+    -e RENOVATE_PLATFORM=gitea \
+    -e RENOVATE_TOKEN= \
+    renovate/renovate:37
